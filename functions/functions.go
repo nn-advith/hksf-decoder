df235ace60dd090108908ba70550a0dbb2cf9167
nn-advith <advith.nn@gmail.com>
2025-09-14 20:05:45 +0530

    single encrypt logic

--- functions/functions.go
+++ functions/functions.go
@@ -3,17 +3,22 @@ package functions
 import (
 	"bytes"
 	"crypto/aes"
+	"crypto/md5"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
 var csheader []int = []int{0, 1, 0, 0, 0, 255, 255, 255, 255, 1, 0, 0, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0}
 var aesKey []byte = []byte("UKu52ePUBwetZ9wNX88o54dnfKRu0T1l") // well known; from game files ig
+
+// DECRYPTION RELATED
 
 // remove pkcs7 padding; used in ECB aes
 func removepadding(data []byte) []byte { // pkcs7 pads N places with byte of value N eg: 2 plcaes = 02, 11 places = 0B
@@ -23,7 +28,6 @@ func removepadding(data []byte) []byte { // pkcs7 pads N places with byte of val
 
 // ECB decryption basic
 func aesDecrypt(ciphertext, key []byte) ([]byte, error) {
-	fmt.Println(aes.BlockSize)
 	if len(ciphertext)%aes.BlockSize != 0 {
 		return nil, fmt.Errorf("ciphertext length must be multiple of %d", aes.BlockSize)
 	}
@@ -31,11 +35,12 @@ func aesDecrypt(ciphertext, key []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	// fmt.Println("before decrypt: md5:", md5.Sum(ciphertext))
 	plaintext := make([]byte, len(ciphertext))
 	for start := 0; start < len(ciphertext); start += aes.BlockSize { // iterate over the ciphertext, per blocak and decrypt
 		block.Decrypt(plaintext[start:start+aes.BlockSize], ciphertext[start:start+aes.BlockSize])
 	}
+	// fmt.Println("before removing padding: md5: ", md5.Sum(plaintext))
 	return removepadding(plaintext), nil
 }
 
@@ -49,8 +54,14 @@ func DecryptSingleFile(infile string) error {
 	if err != nil {
 		return fmt.Errorf("error : fileopen %v", err)
 	}
+	// fmt.Println("before length prefix: md5: ", md5.Sum(data))
 	data = data[len(csheader) : len(data)-1] // remove the csheader; part of every save file and the last byte which is valeue 11
 
+	// prefix := data[0:3]
+	// for _, v := range prefix {
+	// 	fmt.Printf("0x%X ", v)
+	// }
+	// fmt.Println()
 	lc := 0
 	for i := range 5 {
 		lc++
@@ -58,26 +69,27 @@ func DecryptSingleFile(infile string) error {
 			break
 		}
 	}
-
 	data = data[lc:]
-
+	// fmt.Println("before b64 decode: md5:", md5.Sum([]byte(data)))
 	decodeddata, err := base64.StdEncoding.DecodeString(string(data))
 	if err != nil {
 		return fmt.Errorf("error : base64 decode %v", err)
 	}
-	// fmt.Println(string(decodeddata))
 
 	plaintext, err := aesDecrypt(decodeddata, aesKey)
 	if err != nil {
 		return fmt.Errorf("error : decrypt %v", err)
 	}
 
+	// fmt.Println("plaintext: md5:", md5.Sum(plaintext))
 	var formatted bytes.Buffer
 	err = json.Indent(&formatted, []byte(plaintext), "", " ")
 	if err != nil {
 		return fmt.Errorf("error: formatting to json %v", err)
 	}
-	err = os.WriteFile("./OP/"+opname, formatted.Bytes(), 0644)
+	// fmt.Println("before write: md5:", md5.Sum(formatted.Bytes()))
+
+	err = os.WriteFile("./DECODED/"+opname, formatted.Bytes(), 0644)
 	if err != nil {
 		return err
 	}
@@ -108,4 +120,163 @@ func DecryptDirectory(indir string) error {
 		return fmt.Errorf("error while descrypting directory %v", err)
 	}
 	return nil
+}
+
+// ENCRYPTION RELATED
+
+func addPadding(data []byte) []byte {
+	// can cheat by directly assuming 0x0B padding based on knowledge but for fun
+	// dynamic padding
+	// fmt.Println("modulus: ", len(data)%aes.BlockSize)
+	padLen := aes.BlockSize - (len(data) % aes.BlockSize)
+	padSlice := bytes.Repeat([]byte{byte(padLen)}, padLen)
+	data = append(data, padSlice...)
+	return data
+}
+
+func aesEncrypt(plaintext, key []byte) ([]byte, error) {
+	// add padding here
+	plaintext = addPadding(plaintext)
+	// fmt.Println("after padding: md5: ", md5.Sum(plaintext))
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, len(plaintext))
+	for start := 0; start < len(plaintext); start = start + aes.BlockSize {
+		block.Encrypt(ciphertext[start:start+aes.BlockSize], plaintext[start:start+aes.BlockSize])
+	}
+
+	return []byte(ciphertext), nil
+}
+
+func appendLengthPrefix(data []byte) ([]byte, error) {
+	// from length calculate the length prefix with separator
+	// pseudo:
+	// convert length: decimal to binary
+	// if len(length)%7 != 0; then pad with difference at MSB
+	// blocksize = 7
+	// strating from end;
+	// for each block:
+	// 	if ! reach beginning; then MSB=1 followed by blockdata
+	// 	else MSB = 0 followed by blockdata
+	// endfor
+	// convert to byte array
+	length := len(data)
+	blocksize := 7
+	bin := strconv.FormatInt(int64(length), 2)
+	if len(bin)%blocksize != 0 {
+		diff := blocksize - len(bin)%blocksize
+		pref := strings.Repeat("0", diff)
+		bin = pref + bin
+	}
+	lprefix := make([]byte, 0)
+	// fmt.Println(lprefix)
+	for start := len(bin); start >= blocksize; start -= blocksize {
+		temp := bin[start-blocksize : start]
+		if start > blocksize {
+			//not finished yet; add mSB
+
+			temp = "1" + temp
+		} else {
+			//done add separator
+			temp = "0" + temp
+
+		}
+		conv, err := strconv.ParseUint(temp, 2, 8)
+		if err != nil {
+			return []byte{}, fmt.Errorf("conversion error %v", err)
+		}
+		lprefix = append(lprefix, byte(conv))
+	}
+
+	// for _, v := range lprefix {
+	// 	fmt.Printf("0x%X ", v)
+	// }
+	// fmt.Println()
+	data = append(lprefix, data...)
+	return data, nil
+}
+
+func inttobyte(insclice []int) []byte {
+	outslice := make([]byte, len(insclice))
+	for i, v := range insclice {
+		outslice[i] = byte(v)
+	}
+	return outslice
+}
+
+func EncryptSingleFile(infile string) error {
+	splitpath := strings.Split(infile, "/")
+	fname := splitpath[len(splitpath)-1]
+	opname := fmt.Sprintf("%s-encoded.dat", strings.Split(fname, "-decoded")[0])
+
+	fdata, err := os.ReadFile(infile)
+	if err != nil {
+		return fmt.Errorf("error : fileopen %v", err)
+	}
+
+	// fmt.Println("after read: md5:", md5.Sum(fdata))
+	var scrap bytes.Buffer
+	err = json.Compact(&scrap, fdata)
+	if err != nil {
+		return err
+	}
+	// fmt.Println("plaintext: md5:", md5.Sum(scrap.Bytes()))
+	ciphertext, err := aesEncrypt(scrap.Bytes(), aesKey)
+	if err != nil {
+		return fmt.Errorf("error : encrypt %v", err)
+	}
+	// fmt.Println("after encrypt: md5:", md5.Sum(ciphertext))
+
+	encodeddata := base64.StdEncoding.EncodeToString(ciphertext)
+	// fmt.Println("after b64 encode: md5:", md5.Sum([]byte(encodeddata)))
+	prefixed, err := appendLengthPrefix([]byte(encodeddata))
+	if err != nil {
+		return fmt.Errorf("lengthprefix error: %v", err)
+	}
+	prefixed = append(inttobyte(csheader), append(prefixed, []byte{byte(11)}...)...)
+	// fmt.Println("after length prefix: md5: ", md5.Sum(prefixed))
+
+	err = os.WriteFile("./ENCODED/"+opname, prefixed, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CheckFileEquality(files ...string) (bool, error) {
+	equal := true
+	md5file := func(fp string) (string, error) {
+		f, err := os.Open(fp)
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+		h := md5.New()
+		if _, err := io.Copy(h, f); err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%x", h.Sum(nil)), nil
+	}
+
+	md5comp, err := md5file(files[0])
+	if err != nil {
+		return false, fmt.Errorf("error getting md5 %v", err)
+	}
+
+	for i := 1; i < len(files); i++ {
+		temphash, err := md5file(files[i])
+		if err != nil {
+			return false, fmt.Errorf("error getting md5 %v", err)
+		}
+		if temphash != md5comp {
+			equal = false
+			break
+		}
+
+	}
+	return equal, nil
 }
--- main.go
+++ main.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/nn-advith/hksfe/functions"
 )
 
+func createDirs() error {
+	// create DECODED and ENCODED for output
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Join(wd, "DECODED"), 0755)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Join(wd, "ENCODED"), 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 
-	sfpath := os.Args[1]
-	fmt.Println(sfpath)
+	operation := os.Args[1]
+	sfpath := os.Args[2]
+	createDirs()
 	if info, err := os.Stat(sfpath); err != nil {
 		if os.IsNotExist(err) {
 			fmt.Println("path not found")
@@ -24,30 +43,51 @@ func main() {
 		if err != nil {
 			fmt.Println("error: getting abs path")
 		}
-		if info.IsDir() {
-			fmt.Println("directory")
-			err := functions.DecryptDirectory(fullname)
-			if err != nil {
-				fmt.Println("error decrypting directory")
+		if strings.ToLower(operation) == "d" || strings.ToLower(operation) == "decode" {
+
+			if info.IsDir() {
+				fmt.Println("directory mode")
+				err := functions.DecryptDirectory(fullname)
+				if err != nil {
+					fmt.Println("error decrypting directory")
+				}
+				fmt.Fprintf(os.Stdout, "\033[032mSUCCESS\033[0m: decoded files; Go get your GEO!\n")
+			} else {
+				//get file name and verify if it matches pattern
+				// pattern := `^user[1-4]\.dat$`
+
+				// re := regexp.MustCompile(pattern)
+				// if re.MatchString(fname) {
+				// 	fmt.Println("OK")
+				// } else {
+				// 	fmt.Println("not a valid file name; must match user[1-4].dat")
+				// }
+
+				err := functions.DecryptSingleFile(fullname)
+				if err != nil {
+					fmt.Println("error decrypting file")
+				}
+
+				fmt.Fprintf(os.Stdout, "\033[032mSUCCESS\033[0m: decoded into file; Go get your GEO!\n")
+
 			}
-		} else {
-			//get file name and verify if it matches pattern
-			// pattern := `^user[1-4]\.dat$`
-
-			// re := regexp.MustCompile(pattern)
-			// if re.MatchString(fname) {
-			// 	fmt.Println("OK")
-			// } else {
-			// 	fmt.Println("not a valid file name; must match user[1-4].dat")
-			// }
-
-			err = functions.DecryptSingleFile(fullname)
-			if err != nil {
-				fmt.Println("error decrypting file")
+		} else if strings.ToLower(operation) == "e" || strings.ToLower(operation) == "encode" {
+			if info.IsDir() {
+				fmt.Println("directory mode")
+				// err := functions.E(fullname)
+				// if err != nil {
+				// 	fmt.Println("error decrypting directory")
+				// }
+				fmt.Fprintf(os.Stdout, "\033[032mSUCCESS\033[0m: encoded files; Rename files to match user[1-4].dat to use them in game\n")
+			} else {
+				err := functions.EncryptSingleFile(fullname)
+				if err != nil {
+					fmt.Println("error decrypting file")
+				}
+				equal, _ := functions.CheckFileEquality("./DAT/user4.dat", "./ENCODED/user4-encoded.dat")
+				fmt.Println(equal)
+				fmt.Fprintf(os.Stdout, "\033[032mSUCCESS\033[0m: encoded into file; Rename file to match user[1-4].dat to use them in game\n")
 			}
-
-			fmt.Fprintf(os.Stdout, "\033[032mSUCCESS\033[0m: decrypyted into file\n")
-
 		}
 	}
 
